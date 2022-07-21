53559740ffdb22dd5c4ac5aa3672bd840353c41b
Yeon0023 <yeon0023@e.ntu.edu.sg>
2022-07-22 03:51:43 +0800

    3

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:nigel_flutterbasic/view/Login_view.dart';
 import 'firebase_options.dart';
@@ -15,8 +16,40 @@ void main() async {
       theme: ThemeData(
         primarySwatch: Colors.red,
       ),
-      home: const LoginView()
+      home: const HomePage()
     ),
   );
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+   @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home')),
+      body: FutureBuilder(
+        future: Firebase.initializeApp(
+          options: DefaultFirebaseOptions.currentPlatform,
+        ),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+              final user = FirebaseAuth.instance.currentUser;
+              //final emailVerified = user?.emailVerified ?? false;
+              if (user?.emailVerified ?? false){
+                print('You are a verified user');
+              }else {
+                print('You need to verify with your email first');
+              }
+              return const Text('Done');
+            default:
+              return const Text("Loading...");
+          }
+        },
+      ),
+    );
+  }
 }
 
