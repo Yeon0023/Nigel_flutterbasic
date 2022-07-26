35a11008ad863a1cc94192e751fed13345b4336a
Yeon0023 <yeon0023@e.ntu.edu.sg>
2022-07-27 04:46:49 +0800

    270722

---
+++ lib/service/auth/auth_provider.dart
@@ -0,0 +1,16 @@
+import 'package:nigel_flutterbasic/service/auth/auth_user.dart';
+
+//Creation of abtract class
+abstract class AuthProvider {
+  AuthUser? get currentUser;
+  Future<AuthUser> logIn({
+    required String email,
+    required String password,
+  });
+  Future<AuthUser> createUser({
+    required String email,
+    required String password,
+  });
+  Future<void> logOut();
+  Future<void> sendEmailVerification();
+}
